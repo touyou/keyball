8e1f88f04583b16b29e2b76bcedbbeeec8a3b92a
touyou <fujiyou1121@gmail.com>
2025-05-04 16:02:00 +0900

    レイヤー4の際にスクロールモードを自動有効化し、スクロールスナップモードを垂直に設定する機能を追加

--- qmk_firmware/keyboards/keyball/keyball39/keymaps/touyou/keymap.c
+++ qmk_firmware/keyboards/keyball/keyball39/keymaps/touyou/keymap.c
@@ -71,6 +71,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 layer_state_t layer_state_set_user(layer_state_t state) {
   // Auto enable scroll mode when the highest layer is 4
   keyball_set_scroll_mode(get_highest_layer(state) == 4);
+  keyball_set_scrollsnap_mode(KEYBALL_SCROLLSNAP_MODE_VERTICAL);
 
 // Disable auto mouse when the layer is 1
 #ifdef POINTING_DEVICE_AUTO_MOUSE_ENABLE
