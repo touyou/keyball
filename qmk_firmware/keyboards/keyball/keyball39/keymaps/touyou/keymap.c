b8922535b0c41c5cb8111cd7337a094927b236e8
touyou <fujiyou1121@gmail.com>
2025-05-06 00:14:10 +0900

    レイヤー0のキーマップで、CTL_TをALT_Tに変更し、レイヤー4のモードを修正しました

--- qmk_firmware/keyboards/keyball/keyball39/keymaps/touyou/keymap.c
+++ qmk_firmware/keyboards/keyball/keyball39/keymaps/touyou/keymap.c
@@ -26,9 +26,9 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   // Layer 0: Root layer
   [0] = LAYOUT_universal(
     KC_Q, KC_W, KC_E, KC_R, KC_T,     KC_Y, KC_U, KC_I, KC_O, KC_P,
-    CTL_T(KC_A), KC_S, KC_D, KC_F, KC_G,    KC_H, KC_J, KC_K, KC_L, RSFT_T(KC_ENT),
+    ALT_T(KC_A), KC_S, KC_D, KC_F, KC_G,    KC_H, KC_J, KC_K, KC_L, RSFT_T(KC_ENT),
     SFT_T(KC_Z), KC_X, KC_C, KC_V, KC_B,    KC_N, KC_M, KC_COMM, KC_DOT, KC_SLSH,
-    KC_LCTL, KC_LSFT, LT(4,KC_LALT), LGUI_T(KC_LNG2), LT(1,KC_SPC), LT(3,KC_TAB),    LT(1,KC_BSPC), RGUI_T(KC_LNG1), XXXXXXX, XXXXXXX, XXXXXXX, LT(3,KC_ESC)
+    KC_LCTL, KC_LSFT, MO(4), LGUI_T(KC_LNG2), LT(1,KC_SPC), LT(3,KC_TAB),    LT(1,KC_BSPC), RGUI_T(KC_LNG1), XXXXXXX, XXXXXXX, XXXXXXX, LT(3,KC_ESC)
   ),
   // Layer 1: Symbol layer
   [1] = LAYOUT_universal(
